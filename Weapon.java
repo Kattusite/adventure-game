0d6193e6e974fda15469f9b38cb814804fb04793
Mzulli1 <mzulli1@binghamton.edu>
2017-01-14 00:46:49 -0500

    Create Weapon.java

---
+++ Weapon.java
@@ -0,0 +1,21 @@
+public class Weapon(){
+    private String name;
+    private int damage;
+    private int durability;
+
+    public weapon(String name, int damage, int durability)
+        this.name = name;
+        this.damage = damage;
+        this.durability = durability;
+}
+
+// Sween's getter methods
+public String name(){
+    return name;
+}
+public int damage(){
+    return damage;
+
+public int durablility;
+    return durability;
+}
