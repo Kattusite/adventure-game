f9b79115a3c960e545b9bdb67ec57bdbd9e78efa
Mzulli1 <mzulli1@binghamton.edu>
2017-01-14 00:47:30 -0500

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,19 @@
+public class Food{
+    private String name;
+    private int health;
+    private int risk;
+    public Food(String name, int health, int risk){
+        this.name = name;
+        this.health= health;
+        this.risk = risk;
+    }
+public String name(){
+    return name;
+}
+public int health(){
+    return health;
+}
+public int risk(){
+    return risk;
+}
+}
