56c978e87646b62da04e6cc67c9a88caca4b8b7c
Kattusite <Kattusite@users.noreply.github.com>
2017-01-13 23:57:52 -0500

    Create GameConstants.java

---
+++ GameConstants.java
@@ -0,0 +1,6 @@
+public class GameConstants {
+    static final String gameTitle = "Grabthar's Fourth";
+    static final int xResolution = 800;
+    static final int yResolution = 600;
+    static final int borderSize = 10;
+}
